7651bf84095b18094d2784c6c1d688bd39deb878
ARR4NN <arranhardy07@gmail.com>
2023-04-03 19:02:32 +0100

    banners

--- app.js
+++ app.js
@@ -6,8 +6,35 @@ import { handleWebhook } from "./src/EventHandler/stripe-webhook-handler.js";
 // // Imports local
 // import { handleStripeWebhook } from './src/controllers/handleStripeWebhook';
 const app = express();
+
+app.set("trust proxy", true);
+app.use(function (req, res, next) {
+    const allowedOrigins = [
+        "http://localhost:3000",
+        "http://localhost:3006",
+        "https://ronanhost.com",
+        "https://api.ronanhost.com",
+    ];
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+
 app.use(bodyParser.raw({ type: 'application/json' }));
 app.post('/webhooks/stripe', handleWebhook);
+
+import BannerController from './src/models/controllers/BannerController.js';
+app.use('/banner', BannerController);
+
+
 app.get("/", (req, res) => {
     res.send("Hello, world!");
 });

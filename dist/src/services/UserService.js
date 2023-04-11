b637a9f26ae85e370dab2b02da0c8d9c85989dd1
kristoffer <avion@mailfence.com>
2023-04-11 17:52:01 +0200

    Remove dist

--- dist/src/services/UserService.js
+++
@@ -1,9 +0,0 @@
-import BaseService from "../services/Base/BaseService.js";
-export default class UserService extends BaseService {
-    constructor(repository) {
-        super(repository);
-    }
-    async create(data) {
-        return await this.repository.create(data).catch(e => { throw e; });
-    }
-}

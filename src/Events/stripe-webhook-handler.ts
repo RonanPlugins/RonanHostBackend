622e926abd8249100750de222806f50f0392ac6a
kristoffer <avion@mailfence.com>
2023-04-16 00:02:39 +0200

    Fix attempt #403

--- src/Events/stripe-webhook-handler.ts
+++ src/Events/stripe-webhook-handler.ts
@@ -156,7 +156,7 @@ export async function handleWebhook(request, response) {
                         const plan = metadata
                         console.log(plan)
                         const nId = (await findAvailableNode(pteroClient, plan.memory))[0]
-                        console.log(nId)
+                        console.log(nId, plan.memory, pteroClient, await findAvailableNode(pteroClient, plan.memory))
                         const node: Pterodactyl.Node = await pteroClient.getNode(String(nId))
                             .catch(e => {
                                 console.error(e)

1c8ad45aa47c065072ca4f69eabce35779409734
kristoffer <avion@mailfence.com>
2023-04-15 23:31:04 +0200

    Implement node statistics

--- src/controllers/StatisticsController.ts
+++ src/controllers/StatisticsController.ts
@@ -3,6 +3,7 @@ import { Permissions } from "../enum/Permissions.js";
 import { query } from "../util/data/database.js";
 import randomResponse from "../util/message/checkLoggedInFailedResponse.js";
 import pteroClient from "../util/external/builds/PterodactylClient.js"
+import {getTotalAllocatedMemoryByNode} from "../util/nodes/NodeAllocator.js";
 const router = express.Router();
 
 const checkLoggedInAndHasPermission = (neededPermission: Permissions) => {
@@ -26,7 +27,6 @@ const getTableStatus = async (tableName) => {
     return { total: rows.Rows };
 };
 
-
 router.get("/users", checkLoggedInAndHasPermission(Permissions.STATS_READ), async function (req, res, next) {
     const data = await getTableStatus('user');
     res.send(data);
@@ -39,9 +39,9 @@ router.get("/servers", checkLoggedInAndHasPermission(Permissions.STATS_READ), as
     const servers = await pteroClient.getServers()
     res.send({ total: servers.length });
 });
-router.get("/nodes", checkLoggedInAndHasPermission(Permissions.STATS_READ), async function (req, res, next) {
-    const nodes = await pteroClient.getNodes()
-    res.send({ total: nodes.length });
+router.get("/nodes",async function (req, res, next) {
+    const nodes = await getTotalAllocatedMemoryByNode(pteroClient)
+    res.send({ total: nodes.length, nodes: nodes });
 });
 router.get("/locations", checkLoggedInAndHasPermission(Permissions.STATS_READ), async function (req, res, next) {
     const locations = await pteroClient.getLocations()
--- src/util/nodes/NodeAllocator.ts
+++ src/util/nodes/NodeAllocator.ts
@@ -1,4 +1,4 @@
-import {Server} from "@avionrx/pterodactyl-js"
+import {Node, Server} from "@avionrx/pterodactyl-js"
 
 export async function findAvailableNode(client, memoryNeeded): Promise<number[] | null> {
     const availableNodes = [];
@@ -18,5 +18,30 @@ export async function findAvailableNode(client, memoryNeeded): Promise<number[] 
     } catch (error) {
         console.error(error);
         return null;
+    }
+}
+
+interface NodeWithAllocatedMemory extends Node {
+    totalAllocatedMemory: number;
+}
+
+export async function getTotalAllocatedMemoryByNode(client): Promise<NodeWithAllocatedMemory[]> {
+    const nodesWithAllocatedMemory: NodeWithAllocatedMemory[] = [];
+    try {
+        const nodes = await client.getNodes();
+        const servers = await client.getServers();
+
+        for (const node of nodes) {
+            const nodeServers = servers.filter((server: Server) => server.node === node.id);
+            const totalAllocatedMemory = nodeServers.reduce((acc, server) => acc + server.limits.memory, 0);
+
+            const nodeWithAllocatedMemory: NodeWithAllocatedMemory = { ...node, totalAllocatedMemory };
+            nodesWithAllocatedMemory.push(nodeWithAllocatedMemory);
+        }
+
+        return nodesWithAllocatedMemory;
+    } catch (error) {
+        console.error(error);
+        return [];
     }
 }

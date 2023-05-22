a8cb13b91884e36c7f109b2a3ee5b5f455e5ae4b
ARR4NN <arranhardy07@gmail.com>
2023-05-22 22:53:58 +0100

    Add correct CORS origins

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',"https://ronanhost.com","https://www.ronanhost.com"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
